a5748d109b09970061e445be5c3e9c0e64df0476
yuhong02 <myh0510@hotmail.com>
2019-12-28 16:51:25 +0800

    complete

--- src/test/java/RoverTest.java
+++ src/test/java/RoverTest.java
@@ -1,7 +1,12 @@
 import static org.junit.Assert.*;
 import static org.fest.assertions.api.Assertions.assertThat;
 
+import org.junit.runner.RunWith;
 
+import java.lang.Exception;
+
+import org.junit.rules.ExpectedException;
+import org.junit.Rule;
 import org.junit.Test;
 
 
@@ -18,6 +23,41 @@ public class RoverTest {
 	@Test
     public void should_move_forward_when_direction_to_east(){
     	Area area = new Area(10,10);
-    	
+    	Rover rover = new Rover();
+    	rover.land(area, 5, 5, rover.E);
+    	rover.move();
+    	String position = rover.getPosition();
+    	assertThat(position).isEqualTo("65E");
     }
+	@Test
+	public void should_run_left(){
+		Area area = new Area(10,10);
+    	Rover rover = new Rover();
+    	rover.land(area, 5, 5, rover.S);
+    	rover.turnLeft();
+    	assertThat(rover.getPosition()).isEqualTo("55E");
+    	rover.turnLeft();
+    	assertThat(rover.getPosition()).isEqualTo("55N");
+    	rover.turnLeft();
+    	assertThat(rover.getPosition()).isEqualTo("55W");
+    	rover.turnLeft();
+    	assertThat(rover.getPosition()).isEqualTo("55S");
+	}
+	@Test
+	public void should_execute_batch_commands(){
+		Rover rover = new Rover();
+		RoverController roverController = new RoverController(rover);
+		String mission = "10,10,5,5,E,M,L,M";
+		assertThat(roverController.excute(mission)).isEqualTo("66N");
+	}
+    @Rule 
+    public ExpectedException expectedException = ExpectedException.none();
+	@Test
+	public void should_warning_when_land_out_of_area(){
+    	expectedException.expect(IllegalArgumentException.class);
+    	expectedException.expectMessage("x=20 is out of area with 10");
+    	Area area = new Area(10,10);
+    	Rover rover = new Rover();
+    	rover.land(area, 20, 30, Rover.S);
+	}
 }
